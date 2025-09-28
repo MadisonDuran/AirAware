0ee03ca85dd5cddde16a1264625c7df8b483cda7
Thanh Nay <218096274+tin10n@users.noreply.github.com>
2025-09-27 23:08:11 -0400

    added to server.js and added to .gitignore

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,14 @@
 const express = require('express');
+const cors = require('cors')
 require('dotenv').config();
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 
+app.use(cors());
+app.use(express.json());
+
+// Routes
 app.get('/api/test', (req, res) => {
     res.json({ message: 'API is working!'});
 });

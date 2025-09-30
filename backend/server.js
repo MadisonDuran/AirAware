a804ff98692e77975fafe5cfae7c99e05e944cc9
Thanh Nay <218096274+tin10n@users.noreply.github.com>
2025-09-30 17:34:04 -0400

    created route for sumbmission, and also added in .env.example file

--- backend/server.js
+++ backend/server.js
@@ -5,10 +5,12 @@ const express = require('express');
 const axios = require('axios');
 const cors = require('cors');
 require('dotenv').config();
+console.log('Loaded API Key:', process.env.OPENAQ_API_KEY);
 
 const app = express();
 const PORT = process.env.PORT || 5000;
 
+// Middleware
 app.use(cors());
 app.use(express.json());
 

af44eac6b76fae27ed0407580113783afaa00813
Thanh Nay <218096274+tin10n@users.noreply.github.com>
2025-10-06 18:31:38 -0400

    updated routes

--- backend/routes/search.js
+++ backend/routes/search.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const { getCityWeather } = require('../controllers/weatherController');
+const { getCityWeather } = require('../controllers/resultController');
 
 // GET /api/search/:city
 router.get('/:city', getCityWeather);

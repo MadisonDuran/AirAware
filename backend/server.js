a869bc2e840e52ff81ea3baa3cecf0bf5239802f
Edgardo Torres <ejtf99usa@gmail.com>
2025-10-07 09:49:35 -0400

    fixing bugs

--- backend/server.js
+++ backend/server.js
@@ -4,7 +4,7 @@ const axios = require('axios');
 const cors = require('cors');
 const supabase = require('./supabaseClient');
 require('dotenv').config();
-const PORT = process.env.PORT || 4000;
+const PORT = process.env.PORT || 5000;
 
 const path = require('path');
 
